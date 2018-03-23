470b1607a870995e50184d9cedad452c883d52d6
Adrian Silvani <asilvani@Adrian-Silvanis-MacBook-Pro.local>
2018-03-23 04:49:36 +0100

    endday

--- src/muypicky/urls.py
+++ src/muypicky/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from restaurants.views import home, about, contact
+from django.views.generic import TemplateView
+
+from restaurants.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home),
-    url(r'^contact/$', contact),
-    url(r'^about/$', about),
+    url(r'^$', HomeView.as_view(template_name='home.html')),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html')),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html')),
 ]
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,22 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
 import random
 # Create your views here.
 # function based view
 
 
+class HomeView(TemplateView):
+    template_name = 'home.html'
 
-def home(request):
-    randint = random.randint(0, 1000000)
-    context = {"randint": randint}
-    return render(request, "home.html", context)
-
-
-def about(request):
-    context = {}
-    return render(request, "about.html", context)
-
-
-def contact(request):
-    context ={}
-    return render(request, "contact.html", context)
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        randint = random.randint(0, 1000000)
+        context = {"randint": randint}
+        return context
