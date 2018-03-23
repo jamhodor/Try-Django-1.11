6b44ce0f444d21096290fa4ac43ba158e7b47af8
Adrian Silvani <asilvani@Adrian-Silvanis-MacBook-Pro.local>
2018-03-23 02:10:32 +0100

    second commit

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+import random
 # Create your views here.
+# function based view
+
+
+
+def home(request):
+    randint = random.randint(0, 1000000)
+    context = {"randint": randint}
+    return render(request, "home.html", context)
+
+
+def about(request):
+    context = {}
+    return render(request, "about.html", context)
+
+
+def contact(request):
+    context ={}
+    return render(request, "contact.html", context)
